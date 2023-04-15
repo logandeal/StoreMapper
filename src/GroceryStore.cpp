a794f098a35157f21b76b9923858308003253a1e
Edison Briney <edisonbriney@gmail.com>
2023-04-14 22:16:58 -0500

    Created grocerystore class and edge struct

--- src/GroceryStore.cpp
+++ src/GroceryStore.cpp
@@ -1,3 +1,27 @@
 // Singleton
 // Create the adjacency List
 // Stores the items
+#include <iostream>
+#include <map>
+#include <vector>
+
+struct Edge {
+    float weight;
+    std::string name;
+};
+
+class GroceryStore {
+    public: 
+        static GroceryStore& getInstance(){
+            static GroceryStore instance;
+            return instance;
+	    }
+        GroceryStore(GroceryStore const& ) = delete; // copy constructor;
+        GroceryStore& operator=(GroceryStore const&) = delete; // copy assignment
+        GroceryStore(GroceryStore &&) = delete; // move constructor
+        GroceryStore& operator = (GroceryStore &&) = delete; // move assignment
+        ~GroceryStore();
+    private:
+        std::map<std::string, std::vector<Edge>> adjacencyList;
+        GroceryStore(){}; 
+}
