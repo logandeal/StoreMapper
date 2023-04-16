49816f26d3e9bd1edda90dcd309e0f7f44d39788
Ryan Milewski <rmilewski44@gmail.com>
2023-04-16 16:47:06 -0500

    Add headers, update main with initial logic

---
+++ include/ShoppingList.hpp
@@ -0,0 +1,15 @@
+#ifndef SHOPPINGLIST_GUARD
+#define SHOPPINGLIST_GUARD
+#include <iostream>
+#include <vector>
+
+class ShoppingList {
+    public:
+        void addItem();
+        
+
+    private:
+        
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
+#include "../include/GroceryStore.hpp"
 
 
-int main(void) {
+int main(int argv, char** argc) {
+    //will need to initalize a grocery store instance here (singleton)
+    
 
+    std::cout << "Welcome to StoreMapper!" << std::endl;
+
+    int option = 0;
+    while(option != -1) {
+        std::cout << "Please select an option below:\n1) Add Item to List\n2)Remove Item from List\n3)View Path to Items\n4)Exit" << std::endl;
+        std::cin >> option;
+    }
 }
