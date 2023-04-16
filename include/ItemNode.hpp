57288eabb1ba0b65b796d4f070354dcc4ba4fe3f
Ryan Milewski <rmilewski44@gmail.com>
2023-04-16 18:17:25 -0500

    Update Headers with implementation

---
+++ include/ItemNode.hpp
@@ -0,0 +1,19 @@
+#ifndef ITEMNODE_GUARD
+#define ITEMNODE_GUARD
+
+#include <iostream>
+
+class ItemNode {
+    public:
+        ItemNode(std::string name, float price, bool coupon);
+        ~ItemNode();
+        std::string getName();
+        float getPrice();
+    private:
+        std::string itemName;
+        float price;
+        bool validCoupon;
+        //can add more attributes for each item here
+};
+
+#endif
--- include/ShoppingList.hpp
+++ include/ShoppingList.hpp
@@ -2,14 +2,17 @@
 #define SHOPPINGLIST_GUARD
 #include <iostream>
 #include <vector>
-
+#include "ItemNode.hpp"
 class ShoppingList {
     public:
         void addItem();
-        
-
+        void removeItem();
+        ShoppingList();
+        ~ShoppingList();
+        std::vector<ItemNode*> getList();
     private:
-        
+        std::vector<ItemNode*> list;
+        std::vector<ItemNode*> possiblechoices;
 };
 
 #endif
