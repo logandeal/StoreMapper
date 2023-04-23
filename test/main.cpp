0911676d0c9e46c70e328b611b2a08850de7d2b0
Ryan Milewski <rmilewski44@gmail.com>
2023-04-23 18:35:02 -0500

    Add frameworks for more Tests

--- test/main.cpp
+++ test/main.cpp
@@ -4,7 +4,15 @@ TEST_F(ShoppingListTest, IsEmptyInitially) {
     EXPECT_EQ(list->getList().size(),0);
 }
 
+TEST_F(ShoppingListTest, AddItem) {
 
+}
+TEST_F(ShoppingListTest, RemoveItem) {
+
+}
+TEST_F(ShoppingListTest, ViewList) {
+    
+}
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
