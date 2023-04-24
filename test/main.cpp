740934d5d8ed8f9a37100908a646f9f0f42eac88
Ryan Milewski <rsmbby@umsystem.edu>
2023-04-24 19:20:32 +0000

    Add tests for ShoppingList

--- test/main.cpp
+++ test/main.cpp
@@ -5,14 +5,17 @@ TEST_F(ShoppingListTest, IsEmptyInitially) {
 }
 
 TEST_F(ShoppingListTest, AddItem) {
-
+    list->addItem();
+    ASSERT_EQ(list->getList().size(),1);
+    list->addItem();
+    ASSERT_EQ(list->getList().size(),2);
 }
 TEST_F(ShoppingListTest, RemoveItem) {
+    list->addItem();
+    list->removeItem();
+    ASSERT_EQ(list->getList().size(),0);
+}
 
-}
-TEST_F(ShoppingListTest, ViewList) {
-    
-}
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
