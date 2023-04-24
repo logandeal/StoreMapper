6ff5e3b18b2119d06e1947ea332026ff07c486b6
Ryan Milewski <rsmbby@umsystem.edu>
2023-04-24 17:58:03 +0000

    Add more error handling for characters on add/delete functions

--- src/ShoppingList.cpp
+++ src/ShoppingList.cpp
@@ -24,24 +24,31 @@ void ShoppingList::addItem() {
     for(int i = 0; i < (int) this->possiblechoices.size();i++) {
         std::cout << i << ") " << possiblechoices[i]->getName() << " : $" << possiblechoices[i]->getPrice() << std::endl;
     }
-    int input = -1;
+    char input = '0';
     try
     {
         std::cin >> input;
-        if(input < 0 || input >= (int) this->possiblechoices.size()) {
+        if(std::atoi(&input) < 0 || std::atoi(&input) >= (int) this->possiblechoices.size()) {
+            throw std::invalid_argument("Invalid choice, please select again!");
+        }
+        else if(!isdigit(input)) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
     }
     catch(const std::invalid_argument& e) {
         std::cerr << e.what() << "\n";
-        addItem();
+        std::cin.clear();
+        std::cin.ignore(input, '\n');
+        input = -1;
+        return;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        return;
     }
-    this->list.push_back(this->possiblechoices[input]);
-    this->possiblechoices.erase(possiblechoices.begin() + input);
+    this->list.push_back(this->possiblechoices[std::atoi(&input)]);
+    this->possiblechoices.erase(possiblechoices.begin() + std::atoi(&input));
 }
 
 void ShoppingList::removeItem() {
@@ -53,24 +60,30 @@ void ShoppingList::removeItem() {
         std::cout << i << ") " << list[i]->getName() << " : $" << list[i]->getPrice() << std::endl;
     }
 
-    int input = -1;
+    char input = '0';
     try
     {
         std::cin >> input;
-        if(input < 0 || input >= (int)this->list.size()) {
+        if(std::atoi(&input) < 0 || std::atoi(&input) >= (int)this->list.size()) {
+            throw std::invalid_argument("Invalid choice, please select again!");
+        }
+        else if(!isdigit(input)) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
     }
     catch(const std::invalid_argument& e) {
         std::cerr << e.what() << "\n";
-        removeItem();
+        std::cin.clear();
+        std::cin.ignore(input, '\n');
+        return;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        return;
     }
-    this->possiblechoices.push_back(this->list[input]);
-    list.erase(list.begin() + input);
+    this->possiblechoices.push_back(this->list[std::atoi(&input)]);
+    list.erase(list.begin() + std::atoi(&input));
     
 }
 
