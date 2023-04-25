b6aa4a5ee2976c94aaba1e0fc63bee01e5658128
Ryan Milewski <rsmbby@umsystem.edu>
2023-04-25 23:11:40 +0000

    Update ShoppingList Test

--- test/ShoppingList.cpp
+++ test/ShoppingList.cpp
@@ -1,9 +1,82 @@
 #include "../include/ShoppingList.hpp"
-
+//TODO: switch to strings, and actually parse input correctly for given integers
 ShoppingList::ShoppingList() {
     possiblechoices.push_back(new ItemNode("Soda",10,false));
     possiblechoices.push_back(new ItemNode("Cheese",3.4,false));
     possiblechoices.push_back(new ItemNode("Milk",3.99,true));
+    possiblechoices.push_back(new ItemNode("Cookies",4.29,false));
+    possiblechoices.push_back(new ItemNode("Chips",2.49,false));
+    possiblechoices.push_back(new ItemNode("Juice",1.99,false));
+    possiblechoices.push_back(new ItemNode("Yogurt",0.59,true));
+    possiblechoices.push_back(new ItemNode("Eggs",5,false));
+
+    GroceryStore::getInstance().addNode("Soda");
+    GroceryStore::getInstance().addNode("Cheese");
+    GroceryStore::getInstance().addNode("Milk");
+    GroceryStore::getInstance().addNode("Cookies");
+    GroceryStore::getInstance().addNode("Chips");
+    GroceryStore::getInstance().addNode("Juice");
+    GroceryStore::getInstance().addNode("Yogurt");
+    GroceryStore::getInstance().addNode("Eggs");
+
+    GroceryStore::getInstance().addNode("Enter");
+    GroceryStore::getInstance().addNode("A1Left");
+    GroceryStore::getInstance().addNode("A1Right");
+    GroceryStore::getInstance().addNode("A2Left");
+    GroceryStore::getInstance().addNode("A2Right");
+    GroceryStore::getInstance().addNode("A3Left");
+    GroceryStore::getInstance().addNode("A3Right");
+    GroceryStore::getInstance().addNode("Exit");
+
+    Edge e;
+    e.name = "A1Left";
+    e.weight = 2;
+    GroceryStore::getInstance().addEdge("Enter",e);
+    e.name = "A1Right";
+    e.weight = 6;
+    GroceryStore::getInstance().addEdge("Enter",e);
+    e.weight = 1;
+    GroceryStore::getInstance().addEdge("Cheese",e);
+    e.name = "Cheese";
+    e.weight = 5;
+    GroceryStore::getInstance().addEdge("Milk",e);
+    e.name = "Milk";
+    e.weight = 4;
+    GroceryStore::getInstance().addEdge("A1Left",e);
+    e.name = "A2Left";
+    e.weight = 3;
+    GroceryStore::getInstance().addEdge("A1Left",e);
+    e.weight = 1;
+    GroceryStore::getInstance().addEdge("Chips",e);
+    e.name = "Cookies";
+    e.weight = 4;
+    GroceryStore::getInstance().addEdge("Chips",e);
+    e.weight = 2;
+    GroceryStore::getInstance().addEdge("Soda",e);
+    e.name = "A2Right";
+    e.weight = 3;
+    GroceryStore::getInstance().addEdge("Soda",e);
+    GroceryStore::getInstance().addEdge("A1Right",e);
+    GroceryStore::getInstance().addEdge("A3Right",e);
+    e.name = "Juice";
+    e.weight = 5;
+    GroceryStore::getInstance().addEdge("A3Right",e);
+    e.name = "Yogurt";
+    e.weight = 1;
+    GroceryStore::getInstance().addEdge("Juice",e);
+    e.weight = 3;
+    GroceryStore::getInstance().addEdge("Eggs",e);
+    e.name = "A3Left";
+    e.weight = 1;
+    GroceryStore::getInstance().addEdge("Eggs",e);
+    e.weight = 3;
+    GroceryStore::getInstance().addEdge("A2Left",e);
+    e.name = "Exit";
+    e.weight = 4;
+    GroceryStore::getInstance().addEdge("A3Left",e);
+    e.weight = 8;
+    GroceryStore::getInstance().addEdge("A3Right",e);
+    
 }
 
 ShoppingList::~ShoppingList() {
@@ -24,22 +97,21 @@ void ShoppingList::addItem() {
     for(int i = 0; i < (int) this->possiblechoices.size();i++) {
         std::cout << i << ") " << possiblechoices[i]->getName() << " : $" << possiblechoices[i]->getPrice() << std::endl;
     }
-    char input = '0';
+    std::string input = "-1";
     try
     {
         std::cin >> input;
-        if(std::atoi(&input) < 0 || std::atoi(&input) >= (int) this->possiblechoices.size()) {
+        if(hasCharacters(input)) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
-        else if(!isdigit(input)) {
+        if(std::stoi(input) < 0 || std::stoi(input) >= (int) this->possiblechoices.size()) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
     }
     catch(const std::invalid_argument& e) {
         std::cerr << e.what() << "\n";
         std::cin.clear();
-        std::cin.ignore(input, '\n');
-        input = -1;
+        input = "-1";
         return;
     }
     catch(const std::exception& e)
@@ -47,8 +119,8 @@ void ShoppingList::addItem() {
         std::cerr << e.what() << '\n';
         return;
     }
-    this->list.push_back(this->possiblechoices[std::atoi(&input)]);
-    this->possiblechoices.erase(possiblechoices.begin() + std::atoi(&input));
+    this->list.push_back(this->possiblechoices[std::stoi(input)]);
+    this->possiblechoices.erase(possiblechoices.begin() + std::stoi(input));
 }
 
 void ShoppingList::removeItem() {
@@ -60,21 +132,20 @@ void ShoppingList::removeItem() {
         std::cout << i << ") " << list[i]->getName() << " : $" << list[i]->getPrice() << std::endl;
     }
 
-    char input = '0';
+    std::string input = "-1";
     try
     {
         std::cin >> input;
-        if(std::atoi(&input) < 0 || std::atoi(&input) >= (int)this->list.size()) {
+        if(hasCharacters(input)) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
-        else if(!isdigit(input)) {
+        if(std::stoi(input) < 0 || std::stoi(input) >= (int)this->list.size()) {
             throw std::invalid_argument("Invalid choice, please select again!");
         }
     }
     catch(const std::invalid_argument& e) {
         std::cerr << e.what() << "\n";
         std::cin.clear();
-        std::cin.ignore(input, '\n');
         return;
     }
     catch(const std::exception& e)
@@ -82,8 +153,8 @@ void ShoppingList::removeItem() {
         std::cerr << e.what() << '\n';
         return;
     }
-    this->possiblechoices.push_back(this->list[std::atoi(&input)]);
-    list.erase(list.begin() + std::atoi(&input));
+    this->possiblechoices.push_back(this->list[std::stoi(input)]);
+    list.erase(list.begin() + std::stoi(input));
     
 }
 
@@ -91,4 +162,13 @@ void ShoppingList::viewCurrentList() {
     for(ItemNode* n : list) {
         std::cout << n->getName() << "  : " << n->getPrice() << std::endl;
     }
+}
+
+bool hasCharacters(std::string s) {
+    for(auto &character : s) {
+        if(!std::isdigit(character)) {
+            return true;
+        }
+    }
+    return false;
 }
