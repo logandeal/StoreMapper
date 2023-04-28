541f8153f8f4b4f742dde85f87527540436782b9
edison <edisonbriney@gmail.com>
2023-04-28 14:04:14 -0500

    added some unitTests for GroceryStore

--- test/inc/GroceryStoreTest.hpp
+++ test/inc/GroceryStoreTest.hpp
@@ -1,12 +1,11 @@
 #ifndef GROCERYSTORETEST_GUARD
 #define GROCERYSTORETEST_GUARD
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 #include "../../include/GroceryStore.hpp"
 
 class GroceryStoreTest : public ::testing::Test {
     protected:
         void SetUp() override {
-
         }
         void TearDown() override {
             
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,6 @@
 #include "inc/ShoppingListTest.hpp"
+#include "inc/GroceryStoreTest.hpp"
+
 
 TEST_F(ShoppingListTest, IsEmptyInitially) {
     EXPECT_EQ(list->getList().size(),0);
@@ -14,6 +16,25 @@ TEST_F(ShoppingListTest, RemoveItem) {
     list->addItem();
     list->removeItem();
     ASSERT_EQ(list->getList().size(),0);
+}
+
+TEST_F(GroceryStoreTest, AddNode){
+    GroceryStore::getInstance().addNode("Milk");
+    ASSERT_EQ(GroceryStore::getInstance().getMap().size(), 1);
+}
+
+TEST_F(GroceryStoreTest, AddEdge){
+    Edge e;
+    e.name = "Milk";
+    e.weight = 1;
+    
+    GroceryStore::getInstance().addNode("Milk");
+    GroceryStore::getInstance().addNode("Cookies");
+    GroceryStore::getInstance().addEdge("Cookies", e);
+    ASSERT_EQ(GroceryStore::getInstance().getMap().size(), 2);
+    ASSERT_EQ(GroceryStore::getInstance().getMap().at("Cookies").begin(), e);
+    e.name = "Cookies";
+    ASSERT_EQ(GroceryStore::getInstance().getMap().at("Milk").begin(), e);
 }
 
 int main(int argc, char** argv) {
