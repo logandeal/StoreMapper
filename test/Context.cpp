2ccf5bf2264732c639cfcfcb6d161eff27927935
Ryan Milewski <rsmbby@umsystem.edu>
2023-05-01 02:32:53 +0000

    Remove uneeded files

--- test/Context.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/Context.hpp"
-
-void Context::setStrategy(SearchStrategy* strat_)  { 
-    strat = strat_;
-}
-
-void Context::strategy(ShoppingList &shoppingList) {
-    if (strat) strat->search(shoppingList); 
-}     
